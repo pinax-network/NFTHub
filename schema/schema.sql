d8b1797259ff226e66b7b03eee3fa4cccdadf691
mat <mat_lefebvre12@hotmail.com>
2023-01-25 12:32:19 -0500

    Init

---
+++ schema/schema.sql
@@ -0,0 +1,16 @@
+create table if not exists nft
+(
+    id                text not null constraint nft_pk primary key,
+    owneraddress             text,
+    contract_address  text,
+    tokenid           text,
+    txhash            text
+);
+
+create table if not exists cursors
+(
+    id         text not null constraint cursor_pk primary key,
+    cursor     text,
+    block_num  bigint,
+    block_id   text
+);
