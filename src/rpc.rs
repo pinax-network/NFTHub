ca89617745e4d364e368cd197f1b05c5be551b85
mat <mat_lefebvre12@hotmail.com>
2023-02-09 16:51:44 -0500

    rpc

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,4 +1,4 @@
-use substreams_ethereum::{pb::eth, rpc};
+use substreams_ethereum::{ pb::eth, rpc };
 use crate::utils::rpc_data;
 #[derive(Debug)]
 pub struct RpcCallParams {
@@ -18,23 +18,43 @@ pub fn fetch_many_ipfs(params: Vec<RpcCallParams>) -> Vec<Result<Vec<u8>, String
             .collect(),
     };
 
-    let array = rpc::eth_call(&rpc_calls)
-   .responses
-   .iter()
-   .enumerate()
-   .map(|(i, r)| {
-       if r.failed {
-           Err(format!("eth_call failed: {:?}", params[i]))
-       } else {
-           Ok(r.raw.clone())
-       }
-   })
-   .collect(); 
-    return array
+    return rpc
+        ::eth_call(&rpc_calls)
+        .responses.iter()
+        .enumerate()
+        .map(|(i, r)| {
+            if r.failed {
+                Err(format!("eth_call failed: {:?}", params[i]))
+            } else {
+                Ok(r.raw.clone())
+            }
+        })
+        .collect();
 }
-
-
 
 pub fn fetch_ipfs(param: RpcCallParams) -> Result<Vec<u8>, String> {
     return fetch_many_ipfs(vec![param]).into_iter().next().unwrap();
+}
+
+pub fn clean_url(data: String) -> String {
+    if data.contains('\u{0000}') {
+        let newdata = data.replace('\u{0000}', "");
+
+        // create the matches
+        let match1 = "http";
+        let match2 = "ipfs";
+        let match3 = "data";
+        let mut index = 0;
+        if newdata.find(match1).is_some() {
+            index = newdata.find(match1).unwrap();
+        } else if newdata.find(match2).is_some() {
+            index = newdata.find(match2).unwrap();
+        } else if newdata.find(match3).is_some() {
+            index = newdata.find(match3).unwrap();
+        }
+
+        let clean_url = &newdata[index..];
+        return clean_url.to_string();
+    }
+    return data;
 }
